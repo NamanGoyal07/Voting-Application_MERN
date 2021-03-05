1105b04b9a6e8c7cf2e0654eec37ec28976026c8
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-05 14:03:32 +0530

    Error Handling

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const app = express();
+const port = 4000;
+
+app.get('/', (req,res) => {
+    res.json({
+        hello: 'world'
+    });
+})
+
+// Error Handling
+app.use((req,res,next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500).json({
+    err: err.message || 'Something went wrong'
+    })
+});
+
+app.listen(port,
+    console.log(`Server startted in port ${port}`)
+);
+
 
 
