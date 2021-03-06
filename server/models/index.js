14fa2063e33d612458a2415f495ad4577e4b2df3
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-07 01:54:50 +0530

    Database connected, Added models

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,11 @@ mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 
 // mongoose.connect('mongodb://localhost/vote-mern');
+mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true }, ()=>{
+    console.log("Successfully connected to database");
+});
 
-
+module.exports.User = require('./user');
+module.exports.Poll = require('./polls');
 
 
